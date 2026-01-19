daab0377497649d5579d58c49445b4f33d107c17
José Isidro <isidro107109@gmail.com>
2026-01-19 20:33:43 +0000

    login creado

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User 
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from datetime import datetime
+from datetime import datetime ,timedelta
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
@@ -72,6 +72,7 @@ def login():
 
     email = (body.get("email") or "").strip().lower()
     password = body.get("password") or ""
+    remember_me = body.get("remember_me", False)
 
     if not email or not password:
         return jsonify({"msg": "email y password son obligatorios"}), 400
@@ -85,8 +86,13 @@ def login():
     user.last_login_at = datetime.utcnow()
     db.session.commit()
 
+    if remember_me:
+        expires = timedelta(days=30)
+    else:
+        expires = timedelta(hours=24)
+
     # crea token
-    access_token = create_access_token(identity=str(user.id))
+    access_token = create_access_token(identity=str(user.id),expires_delta=expires)
 
     return jsonify({
         "access_token": access_token,
