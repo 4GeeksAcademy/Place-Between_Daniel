96d800c731697669cf88e2aad542724a40576128
José Isidro <isidro107109@gmail.com>
2026-01-19 20:11:53 +0000

    Back End login creado

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,6 +6,7 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from datetime import datetime
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -64,3 +65,30 @@ def register():
         "msg": "Usuario creado",
         "user": user.serialize()
     }), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    body = request.get_json(silent=True) or {}
+
+    email = (body.get("email") or "").strip().lower()
+    password = body.get("password") or ""
+
+    if not email or not password:
+        return jsonify({"msg": "email y password son obligatorios"}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not user.check_password(password):
+        return jsonify({"msg": "Credenciales inválidas"}), 401
+
+
+    # actualiza last_login_at
+    user.last_login_at = datetime.utcnow()
+    db.session.commit()
+
+    # crea token
+    access_token = create_access_token(identity=str(user.id))
+
+    return jsonify({
+        "access_token": access_token,
+        "user": user.serialize()
+    }), 200
